dad7949e882bf7ede430cdb8ceeb4ec4279896d7
otreblan <otreblain@gmail.com>
2020-04-19 08:47:52 -0500

    Add pairSort

--- include/pairSort.hpp
+++ include/pairSort.hpp
@@ -14,32 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with hello.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <iostream>
-#include <iterator>
+#pragma once
 
-#include <josephus.hpp>
-#include <pairSort.hpp>
+#include <vector>
 
-int main()
-{
-	std::cout
-		<< "Josephus(41, 3)\n"
-		<< josephus(41, 3) << '\n';
-
-	std::cout << '\n';
-
-	std::vector<int> arr1 = {2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8};
-	std::vector<int> arr2 = {2, 1, 8, 3};
-
-	std::vector<int> arr3 = pairSort(arr1, arr2);
-
-	std::cout << "Pair sort:\n";
-
-	std::copy(arr3.begin(), arr3.end(),
-		std::ostream_iterator<int>(std::cout, " "));
-
-	std::cout << '\n';
-
-	return 0;
-}
-
+std::vector<int> pairSort(std::vector<int>& arr1, std::vector<int>& arr2);
